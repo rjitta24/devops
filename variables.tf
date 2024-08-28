f3ba956637f9b14568d0d86f890adb11c267432a
rjitta24 <62370074+rjitta24@users.noreply.github.com>
2024-08-28 22:08:29 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,39 +1,35 @@
-output "resource_group_name" { 
-  description = "Resource group name." 
-  value       = azurerm_resource_group.main_rg.name
+variable "resource_group_location" { 
+  type        = string 
+  default     = "eastus" 
+  description = "Location of the resource group." 
 } 
 
-output "virtual_network_name" { 
-  description = "Virtual network name." 
-  value       = azurerm_virtual_network.primary_vnet.name
+variable "resource_group_name_prefix" { 
+  type        = string 
+  default     = "rg" 
+  description = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription." 
 } 
 
-output "subnet_name_1" { 
-  description = "Subnet 1 name." 
-  value       = azurerm_subnet.subnet_one.name
+variable "vm_size" { 
+  type        = string 
+  default     = "Standard_B1s" 
+  description = "The size of the Virtual Machine." 
 } 
 
-output "subnet_name_2" { 
-  description = "Subnet 2 name." 
-  value       = azurerm_subnet.subnet_two.name 
-} 
-
-output "vm_name" { 
-  description = "Virtual Machine name." 
-  value       = azurerm_linux_virtual_machine.web_vm.name 
-} 
-
-output "vm_public_ip" { 
-  description = "Public IP address of the Virtual Machine." 
-  value       = azurerm_public_ip.vm_public_ip.ip_address 
-} 
-
-output "vm_private_ip" { 
-  description = "Private IP address of the Virtual Machine." 
-  value       = azurerm_network_interface.vm_nic.private_ip_address 
-} 
-
-output "admin_username" { 
+variable "admin_username" { 
+  type        = string 
+  default     = "azureuser" 
   description = "Admin username for the Virtual Machine." 
-  value       = var.admin_username 
+} 
+
+variable "ssh_public_key_path" { 
+  type        = string 
+  default     = "~/.ssh/id_rsa.pub" 
+  description = "Path to the SSH public key to access the Virtual Machine." 
+} 
+
+variable "environment" { 
+  type        = string 
+  default     = "development" 
+  description = "The environment in which the VM is deployed." 
 }
